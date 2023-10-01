4fc679c3cc18bf84e20098344ce67b2b4a690c20
mangeshahireakaXylus11 <97844580+xylus11@users.noreply.github.com>
2023-10-01 21:18:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	let first=document.getElementById("first").value;
+	let last=document.getElementById("last").value;
+	// console.log(first);
+	alert(first+" "+last);
 }
